21695378e9d65308414feef8905bd4253f3844fe
Emmanuel M. Lartey <emmanuel.lartey@accertix.com>
2022-11-12 08:00:59 +0000

    added login view and minimal home

---
+++ src/Entry.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
+
+import Login from "./components/Login";
+import Home from "./components/Home";
+
+const Entry = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/login' element={<Login />} />
+                <Route path='/' element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default Entry
